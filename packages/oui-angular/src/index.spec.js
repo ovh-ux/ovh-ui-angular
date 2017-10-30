a2db60824284978bb9fb1c9d0a4c715f693b2717
Kevin Manson <kevin.manson@corp.ovh.com>
2017-10-30 12:34:01 -0400

    feat(dropdown): add new component

--- packages/oui-angular/src/index.spec.js
+++ packages/oui-angular/src/index.spec.js
@@ -5,6 +5,7 @@ loadTests(require.context('../../oui-message/src/', true, /.*((\.spec)|(index))$
 loadTests(require.context('../../oui-radio/src/', true, /.*((\.spec)|(index))$/))
 loadTests(require.context('../../oui-radio-group/src/', true, /.*((\.spec)|(index))$/))
 loadTests(require.context('../../oui-loader/src/', true, /.*((\.spec)|(index))$/))
+loadTests(require.context('../../oui-dropdown/src/', true, /.*((\.spec)|(index))$/))
 
 function loadTests (context) {
   context.keys().forEach(context)
