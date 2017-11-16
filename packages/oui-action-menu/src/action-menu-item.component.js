27773befb6f8acc48ebc9d92e0264c9fcd849fca
Kevin Manson <kevin.manson@corp.ovh.com>
2017-11-16 18:54:47 -0500

    feat(ouiActionMenu): refactor action menu

--- packages/oui-action-menu/src/action-menu-item.component.js
+++ packages/oui-action-menu/src/action-menu-item.component.js
@@ -5,7 +5,8 @@ export default {
   bindings: {
     onClick: '&?',
     href: '@?',
-    external: '@?',
+    divider: '<?',
+    external: '<?',
     text: '@'
   }
 }
--- packages/oui-action-menu/src/action-menu-item.controller.js
+++ packages/oui-action-menu/src/action-menu-item.controller.js
@@ -1,27 +1,6 @@
 import { addBooleanParameter } from '@oui-angular/common/component-utils'
-
-const baseClass = 'oui-action-menu__item'
-const dividerClass = 'oui-action-menu__item_divider'
-const buttonClasses = 'oui-button oui-button_action-menu'
-
-const linkTemplate = (dividerClass, external) => `
-  <li class="${baseClass} ${dividerClass}">
-    <a
-      class="${buttonClasses}"
-      data-ng-href="{{::$ctrl.href}}"${external ? ' target="_blank" rel="noopener"' : ''}>
-      <span data-ng-bind="::$ctrl.text"></span>
-      ${external ? '<i class="oui-icon oui-icon-external_link" aria-hidden="true"></i>' : ''}
-    </a>
-  </li>`
-
-const buttonTemplate = dividerClass => `
-  <li class="${baseClass} ${dividerClass}">
-    <button
-      class="${buttonClasses}"
-      type="button"
-      data-ng-click="$ctrl.onClick()"
-      data-ng-bind="::$ctrl.text"></button>
-  </li>`
+import buttomTemplate from './action-menu-item-button.html'
+import linkTemplate from './action-menu-item-link.html'
 
 export default class {
   constructor ($attrs, $compile, $element, $scope) {
@@ -36,17 +15,20 @@ export default class {
   $onInit () {
     addBooleanParameter(this, 'divider')
     addBooleanParameter(this, 'external')
+
+    if (this.external) {
+      this.linkTarget = '_blank'
+      this.linkRel = 'noopener'
+    }
   }
 
   $postLink () {
     let compiled
 
-    const dividerClassName = this.divider ? dividerClass : ''
-
     if (this.$attrs.onClick) {
-      compiled = this.$compile(buttonTemplate(dividerClassName))
+      compiled = this.$compile(buttomTemplate)
     } else {
-      compiled = this.$compile(linkTemplate(dividerClassName, this.external))
+      compiled = this.$compile(linkTemplate)
     }
 
     this.$element.replaceWith(compiled(this.$scope))
