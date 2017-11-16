751329b71b0b2be333c392c0aa8bb53cbbee1332
Kevin Manson <kevin.manson@corp.ovh.com>
2017-11-16 17:59:18 -0500

    feat(ouiDropdown): code review

--- packages/common/component-utils.js
+++ packages/common/component-utils.js
@@ -1,11 +1,21 @@
 import _ from 'lodash'
 
 export function addBooleanParameter (controller, parameterName) {
-  if (_.has(controller.$attrs, parameterName) && _.isEmpty(controller.$attrs[parameterName])) {
+  if (angular.isDefined(controller.$attrs[parameterName]) &&
+    !_.isBoolean(controller[parameterName])) {
     controller[parameterName] = true
   }
 }
 
+export function addDefaultParameter (controller, parameterName, defaultValue) {
+  if (!angular.isDefined(controller.$attrs[parameterName]) ||
+    (angular.isDefined(controller.$attrs[parameterName] &&
+      controller.$attrs[parameterName].trim() === ''))) {
+    controller[parameterName] = defaultValue
+  }
+}
+
 export default {
-  addBooleanParameter
+  addBooleanParameter,
+  addDefaultParameter
 }
--- packages/oui-action-menu/src/action-menu-item.controller.js
+++ packages/oui-action-menu/src/action-menu-item.controller.js
@@ -1,14 +1,54 @@
 import { addBooleanParameter } from '@oui-angular/common/component-utils'
 
+const baseClass = 'oui-action-menu__item'
+const dividerClass = 'oui-action-menu__item_divider'
+const buttonClasses = 'oui-button oui-button_action-menu'
+
+const linkTemplate = (dividerClass, external) => `
+  <li class="${baseClass} ${dividerClass}">
+    <a
+      class="${buttonClasses}"
+      data-ng-href="{{::$ctrl.href}}"${external ? ' target="_blank" rel="noopener"' : ''}>
+      <span data-ng-bind="::$ctrl.text"></span>
+      ${external ? '<i class="oui-icon oui-icon-external_link" aria-hidden="true"></i>' : ''}
+    </a>
+  </li>`
+
+const buttonTemplate = dividerClass => `
+  <li class="${baseClass} ${dividerClass}">
+    <button
+      class="${buttonClasses}"
+      type="button"
+      data-ng-click="$ctrl.onClick()"
+      data-ng-bind="::$ctrl.text"></button>
+  </li>`
+
 export default class {
-  constructor ($attrs) {
+  constructor ($attrs, $compile, $element, $scope) {
     'ngInject'
 
     this.$attrs = $attrs
+    this.$compile = $compile
+    this.$element = $element
+    this.$scope = $scope
   }
 
   $onInit () {
     addBooleanParameter(this, 'divider')
     addBooleanParameter(this, 'external')
+  }
+
+  $postLink () {
+    let compiled
+
+    const dividerClassName = this.divider ? dividerClass : ''
+
+    if (this.$attrs.onClick) {
+      compiled = this.$compile(buttonTemplate(dividerClassName))
+    } else {
+      compiled = this.$compile(linkTemplate(dividerClassName, this.external))
+    }
+
+    this.$element.replaceWith(compiled(this.$scope))
   }
 }
