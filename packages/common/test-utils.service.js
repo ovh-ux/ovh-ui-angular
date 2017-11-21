da0c56d583faf6827a4ae0c30ae0dc40068f85a8
Axel Peter <axel.peter@corp.ovh.com>
2017-11-21 13:25:00 +0100

    :tractor: chore(oui-button): code review

---
+++ packages/common/test-utils.service.js
@@ -0,0 +1,25 @@
+class TestUtils {
+  constructor ($compile, $rootScope) {
+    'ngInject'
+
+    this.$compile = $compile
+    this.$rootScope = $rootScope
+  }
+
+  compileTemplate (template, context = {}) {
+    const scope = this.$rootScope.$new(true)
+    angular.merge(scope, { $ctrl: context })
+    const element = this.$compile(template)(scope)
+    scope.$digest()
+
+    return { element, scope }
+  }
+
+  getController (componentName, template, context) {
+    const element = this.compileTemplate(template, context)
+    return element.controller(componentName)
+  }
+}
+
+angular.module('oui.test-utils', [])
+  .service('TestUtils', TestUtils)
--- packages/oui-button/src/index.spec.js
+++ packages/oui-button/src/index.spec.js
@@ -1,10 +1,13 @@
 describe('ouiButton', () => {
   let $componentController
+  let testUtils
 
   beforeEach(angular.mock.module('oui.button'))
+  beforeEach(angular.mock.module('oui.test-utils'))
 
-  beforeEach(inject((_$componentController_) => {
+  beforeEach(inject((_$componentController_, _TestUtils_) => {
     $componentController = _$componentController_
+    testUtils = _TestUtils_
   }))
 
   describe('Controller', () => {
@@ -20,90 +23,55 @@ describe('ouiButton', () => {
   })
 
   describe('Component', () => {
-    let $rootScope
-    let $compile
-
-    beforeEach(inject((_$rootScope_, _$compile_) => {
-      $rootScope = _$rootScope_
-      $compile = _$compile_
-    }))
-
     it('should display a button with value of attribute text, and is type="button" by default', () => {
-      let element = angular.element('<oui-button text="foo"></oui-button>')
-      let scope = $rootScope.$new()
+      const component = testUtils.compileTemplate('<oui-button text="foo"></oui-button>')
+      const button = component.element.find('button').eq(0)
 
-      $compile(element)(scope)
-      scope.$digest()
-
-      let button = element.find('button').eq(0)
       expect(button.text().trim()).toBe('foo')
       expect(button.attr('type')).toBe('button')
     })
 
     it('should have an attribute id and name on the button, and removed on the root component', () => {
-      let element = angular.element('<oui-button id="foo" name="bar"></oui-button>')
-      let scope = $rootScope.$new()
+      const component = testUtils.compileTemplate('<oui-button id="foo" name="bar"></oui-button>')
+      const button = component.element.find('button').eq(0)
 
-      $compile(element)(scope)
-      scope.$digest()
-
-      let button = element.find('button').eq(0)
-      expect(element.attr('id')).toBe(undefined)
+      expect(component.element.attr('id')).toBe(undefined)
       expect(button.attr('id')).toBe('foo')
 
-      expect(element.attr('name')).toBe(undefined)
+      expect(component.element.attr('name')).toBe(undefined)
       expect(button.attr('name')).toBe('bar')
     })
 
     it('should have an attribute aria-label on the button, and removed on the root component', () => {
-      let element = angular.element('<oui-button aria-label="foo"></oui-button>')
-      let scope = $rootScope.$new()
+      const component = testUtils.compileTemplate('<oui-button aria-label="foo"></oui-button>')
 
-      $compile(element)(scope)
-      scope.$digest()
-
-      expect(element.attr('aria-label')).toBe(undefined)
-      expect(element.find('button').eq(0).attr('aria-label')).toBe('foo')
+      expect(component.element.attr('aria-label')).toBe(undefined)
+      expect(component.element.find('button').eq(0).attr('aria-label')).toBe('foo')
     })
 
     it('should have a primary next step button', () => {
-      let element = angular.element('<oui-button text="foo" variant="primary" variant-nav="next"></oui-button>')
-      let scope = $rootScope.$new()
+      const component = testUtils.compileTemplate('<oui-button text="foo" variant="primary" variant-nav="next"></oui-button>')
+      const button = component.element.find('button').eq(0)
 
-      $compile(element)(scope)
-      scope.$digest()
-
-      let button = element.find('button').eq(0)
       expect(button.hasClass('oui-button_primary')).toBe(true)
       expect(button.hasClass('oui-button_icon-right')).toBe(true)
     })
 
     it('should have a disabled submit button', () => {
-      let element = angular.element('<oui-button text="foo" type="submit" disabled></oui-button>')
-      let scope = $rootScope.$new()
+      const component = testUtils.compileTemplate('<oui-button text="foo" type="submit" disabled></oui-button>')
+      const button = component.element.find('button').eq(0)
 
-      $compile(element)(scope)
-      scope.$digest()
-
-      let button = element.find('button').eq(0)
       expect(button.attr('disabled')).toBe('disabled')
       expect(button.attr('type')).toBe('submit')
     })
 
     it('should call function of onClick attribute, when button is clicked', () => {
-      let element = angular.element('<oui-button text="foo" on-click="onClickTest()"></oui-button>')
-      let scope = $rootScope.$new()
+      const component = testUtils.compileTemplate('<oui-button text="foo" on-click="$ctrl.onClickTest()"></oui-button>', {
+        onClickTest: jasmine.createSpy('onClick')
+      })
 
-      scope.clickTest = false
-      scope.onClickTest = () => {
-        scope.clickTest = true
-      }
-
-      $compile(element)(scope)
-      scope.$digest()
-
-      element.find('button').eq(0).triggerHandler('click')
-      expect(scope.clickTest).toBe(true)
+      component.element.find('button').eq(0).triggerHandler('click')
+      expect(component.scope.$ctrl.onClickTest).toHaveBeenCalled()
     })
   })
 })
