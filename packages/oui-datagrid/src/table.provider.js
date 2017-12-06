7b7f7b2093726e888d5d6a5b245975732596ec33
Jimmy Fortin <jimmy.fortin@corp.ovh.com>
2017-12-06 11:31:07 -0500

    fix(oui-datagrid): rename css classes
    Signed-off-by: Jimmy Fortin <jimmy.fortin@corp.ovh.com>

--- packages/oui-datagrid/src/table.provider.js
+++ packages/oui-datagrid/src/table.provider.js
@@ -1,21 +1,21 @@
 export default class {
     constructor () {
         this.classes = {
-            tablePanel: "oui-table-panel",
-            table: "oui-table oui-table_responsive",
-            thead: "oui-table__headers",
-            tbody: "oui-table__body",
-            tr: "oui-table__row",
-            th: "oui-table__header",
-            td: "oui-table__cell",
-            sortable: "oui-table__cell_sortable oui-table__cell_sortable-asc",
-            sorted: "oui-table__cell_sorted",
-            sortableAsc: "oui-table__cell_sortable-asc",
-            sortableDesc: "oui-table__cell_sortable-desc",
-            closed: "oui-table__row_closed",
-            emptyTable: "oui-table-empty",
-            thSelector: "oui-table__header_selector",
-            tdSelector: "oui-table__cell_selector"
+            tablePanel: "oui-datagrid-panel",
+            table: "oui-datagrid oui-datagrid_responsive",
+            thead: "oui-datagrid__headers",
+            tbody: "oui-datagrid__body",
+            tr: "oui-datagrid__row",
+            th: "oui-datagrid__header",
+            td: "oui-datagrid__cell",
+            sortable: "oui-datagrid__header_sortable oui-datagrid__header_sortable-asc",
+            sorted: "oui-datagrid__cell_sorted",
+            sortableAsc: "oui-datagrid__header_sortable-asc",
+            sortableDesc: "oui-datagrid__header_sortable-desc",
+            closed: "oui-datagrid__row_closed",
+            emptyTable: "oui-datagrid-empty",
+            thSelector: "oui-datagrid__header_selector",
+            tdSelector: "oui-datagrid__cell_selector"
         };
         this.pageSize = 50;
         this.pageSizes = [
@@ -33,28 +33,28 @@ export default class {
 
     /**
      * Example of list of CSS classes:
-     * - table: oui-table
-     * - thead: oui-table__headers
-     * - tr: oui-table__row
-     * - th: oui-table__header
-     * - tbody: oui-table__body
-     * - td: oui-table__cell
+     * - table: oui-datagrid
+     * - thead: oui-datagrid__headers
+     * - tr: oui-datagrid__row
+     * - th: oui-datagrid__header
+     * - tbody: oui-datagrid__body
+     * - td: oui-datagrid__cell
      *
      * Sorting classes
-     * - sortable(sortable, sorting not active): oui-table__cell_sortable oui-table__cell_sortable-asc
-     * - sorted(sortable, sorting is active): oui-table__cell_sorted
-     * - sortableAsc(ascendant sorting is active): oui-table__cell_sortable-asc
-     * - sortableDesc(descendant sorting is active): oui-table__cell_sortable-desc
+     * - sortable(sortable, sorting not active): oui-datagrid__cell_sortable oui-datagrid__cell_sortable-asc
+     * - sorted(sortable, sorting is active): oui-datagrid__cell_sorted
+     * - sortableAsc(ascendant sorting is active): oui-datagrid__cell_sortable-asc
+     * - sortableDesc(descendant sorting is active): oui-datagrid__cell_sortable-desc
      *
      * Empty placeholder
-     * - emptyTable: oui-table-empty
+     * - emptyTable: oui-datagrid-empty
      *
      * Responsive modifiers
-     * - closed(open row on phone screens): oui-table__row_closed
+     * - closed(open row on phone screens): oui-datagrid__row_closed
      *
      * Selection
-     * - thSelector: oui-table__header_selector
-     * - tdSelector: oui-table__cell_selector
+     * - thSelector: oui-datagrid__header_selector
+     * - tdSelector: oui-datagrid__cell_selector
      */
     setCssConfig (classes) {
         this.classes = classes;
