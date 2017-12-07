f4ff4bebe92804bfd3383b2cd2d187f34e2a05aa
mathieu tremblay <mathieu.tremblay@corp.ovh.com>
2017-12-06 23:51:26 -0500

    feat(oui-message): Remove useless features and standardize

--- packages/oui-message/src/index.spec.js
+++ packages/oui-message/src/index.spec.js
@@ -1,153 +1,109 @@
 import { filter } from "lodash";
 
 describe("oui-message: ", () => {
-    let $compile;
-    let $scope;
-    const message = "My message";
-    let textTranscludeElement;
-    let textBindingElement;
 
+    let TestUtils;
     beforeEach(angular.mock.module("oui.message"));
+    beforeEach(angular.mock.module("oui.test-utils"));
 
-    beforeEach(angular.mock.inject((_$rootScope_, _$compile_) => {
-        $scope = _$rootScope_;
-        $compile = _$compile_;
+    beforeEach(inject((_TestUtils_) => {
+        TestUtils = _TestUtils_;
     }));
 
-    describe("When success type, it", () => {
-        beforeEach(() => {
-            textTranscludeElement = compileDirective(`<oui-message type="success">${message}</oui-message>`);
-            textBindingElement = compileDirective(`<oui-message type="success" message="${message}"></oui-message>`);
-        });
+    describe("Component", () => {
 
         it("should display text message from transclusion", () => {
-            expect(getTextMessageFromTransclusion(textTranscludeElement)).toBe(message);
+            const message = "My message";
+            const element = TestUtils.compileTemplate(`<oui-message>${message}</oui-message>`);
+
+            expect(getBody(element)).toBe(message);
         });
 
-        it("should display text message from binding", () => {
-            expect(getTextMessageFromBinding(textBindingElement)).toBe(message);
+        describe("when info type", () => {
+            it("should display info icon", () => {
+                const element = TestUtils.compileTemplate("<oui-message type=\"info\"></oui-message>");
+
+                expect(getIcons(element, "oui-icon-info_circle").length).toBe(1);
+            });
+
+            it("should display close button", () => {
+                const element = TestUtils.compileTemplate("<oui-message type=\"info\"></oui-message>");
+
+                expect(getCloseButton(element)).toBeDefined();
+            });
+
+            it("should dismiss message when close button is clicked", () => {
+                const element = TestUtils.compileTemplate("<oui-message type=\"info\"></oui-message>");
+
+                getCloseButton(element).click();
+
+                expect(isMessageDisplayed(element)).toBe(false);
+            });
+
+            it("should assign binding value to aria-label attribute on close button", () => {
+                const closeButtonLabel = "My label";
+                const element = TestUtils.compileTemplate(`<oui-message type=\"info\" aria-close-button-label="${closeButtonLabel}"></oui-message>`);
+
+                expect(angular.element(getCloseButton(element)).attr("aria-label")).toBe(closeButtonLabel);
+            });
+
+
+            it("should raise on-dismissed event after close button is clicked", () => {
+                const onDismissedSpy = jasmine.createSpy("onDismissedSpy");
+                const element = TestUtils.compileTemplate("<oui-message type=\"info\" on-dismissed=\"$ctrl.onDismissed()\"></oui-message>", {
+                    onDismissed: onDismissedSpy
+                });
+
+                getCloseButton(element).click();
+
+                expect(onDismissedSpy).toHaveBeenCalled();
+            });
         });
 
-        it("should display success icon", () => {
-            expect(getIcons(textBindingElement, "oui-icon-success_circle").length).toBe(1);
+        describe("when error type", () => {
+            let element;
+            beforeEach(() => {
+                element = TestUtils.compileTemplate("<oui-message type=\"error\"></oui-message>");
+            });
+
+            it("should display error icon", () => {
+                expect(getIcons(element, "oui-icon-error_circle").length).toBe(1);
+            });
+
+            it("should not display close button", () => {
+                expect(getCloseButton(element)).not.toBeDefined();
+            });
         });
 
-        it("should display close button", () => {
-            expect(getCloseButton(textBindingElement)).toBeDefined();
-        });
+        describe("when warning type", () => {
+            let element;
+            beforeEach(() => {
+                element = TestUtils.compileTemplate("<oui-message type=\"warning\"></oui-message>");
+            });
 
-        it("should close message when close button is clicked", () => {
-            getCloseButton(textBindingElement).click();
+            it("should display warning icon", () => {
+                expect(getIcons(element, "oui-icon-warning_circle").length).toBe(1);
+            });
 
-            expect(messageExist(textBindingElement)).toBe(false);
-        });
-    });
-
-    describe("When error type, it", () => {
-        beforeEach(() => {
-            textTranscludeElement = compileDirective(`<oui-message type="error">${message}</oui-message>`);
-            textBindingElement = compileDirective(`<oui-message type="error" message="${message}"></oui-message>`);
-        });
-
-        it("should display text message from transclusion", () => {
-            expect(getTextMessageFromTransclusion(textTranscludeElement)).toBe(message);
-        });
-
-        it("should display text message from binding", () => {
-            expect(getTextMessageFromBinding(textBindingElement)).toBe(message);
-        });
-
-        it("should display error icon", () => {
-            expect(getIcons(textBindingElement, "oui-icon-error_circle").length).toBe(1);
-        });
-
-        it("should not display close button", () => {
-            expect(getCloseButton(textBindingElement)).not.toBeDefined();
-        });
-    });
-
-    describe("When info type, it", () => {
-        beforeEach(() => {
-            textTranscludeElement = compileDirective(`<oui-message type="info">${message}</oui-message>`);
-            textBindingElement = compileDirective(`<oui-message type="info" message="${message}"></oui-message>`);
-        });
-
-        it("should display text message from transclusion", () => {
-            expect(getTextMessageFromTransclusion(textTranscludeElement)).toBe(message);
-        });
-
-        it("should display text message from binding", () => {
-            expect(getTextMessageFromBinding(textBindingElement)).toBe(message);
-        });
-
-        it("should display info icon", () => {
-            expect(getIcons(textBindingElement, "oui-icon-info_circle").length).toBe(1);
-        });
-
-        it("should display close button", () => {
-            expect(getCloseButton(textBindingElement)).toBeDefined();
-        });
-
-        it("should close message when close button is clicked", () => {
-            getCloseButton(textBindingElement).click();
-
-            expect(messageExist(textBindingElement)).toBe(false);
-        });
-    });
-
-    describe("When warning type, it", () => {
-        beforeEach(() => {
-            textTranscludeElement = compileDirective(`<oui-message type="warning">${message}</oui-message>`);
-            textBindingElement = compileDirective(`<oui-message type="warning" message="${message}"></oui-message>`);
-        });
-
-        it("should display text message from transclusion", () => {
-            expect(getTextMessageFromTransclusion(textTranscludeElement)).toBe(message);
-        });
-
-        it("should display text message from binding", () => {
-            expect(getTextMessageFromBinding(textBindingElement)).toBe(message);
-        });
-
-        it("should display warning icon", () => {
-            expect(getIcons(textBindingElement, "oui-icon-warning_circle").length).toBe(1);
-        });
-
-        it("should not display close button", () => {
-            expect(getCloseButton(textBindingElement)).not.toBeDefined();
+            it("should not display close button", () => {
+                expect(getCloseButton(element)).not.toBeDefined();
+            });
         });
     });
-
-    function compileDirective (template, scope) {
-        angular.extend($scope, angular.copy(scope));
-        const element = $compile(template)($scope);
-        $scope.$digest();
-        return element;
-    }
 
     function getCloseButton (element) {
         return getElementsByClassName(element, "button", "oui-message__close-button")[0];
     }
 
-    function getTextTransclude (element) {
-        return getElementsByClassName(element, "div", "oui-message__body")[0];
-    }
-
-    function getTextMessageFromTransclusion (element) {
-        return angular.element(getTextTransclude(element)).text();
-    }
-
-    function getTextMessageFromBinding (element) {
-        const transcludeElement = angular.element(getTextTransclude(element));
-
-        return angular.element(transcludeElement.children()[0]).text();
+    function getBody (element) {
+        return angular.element(getElementsByClassName(element, "div", "oui-message__body")[0]).text();
     }
 
     function getIcons (element, className) {
         return getElementsByClassName(element, "i", className);
     }
 
-    function messageExist (element) {
+    function isMessageDisplayed (element) {
         return getElementsByClassName(element, "div", "oui-message").length > 0;
     }
 
