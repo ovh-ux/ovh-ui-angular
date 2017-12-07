9d567fb70151d3f08791d55645d33e10745dcd30
Jimmy Fortin <jimmy.fortin@corp.ovh.com>
2017-12-07 15:37:24 -0500

    refactor(oui-datagrid): directive to components
    Signed-off-by: Jimmy Fortin <jimmy.fortin@corp.ovh.com>

---
+++ packages/oui-datagrid/src/cell/cell.controller.js
@@ -0,0 +1,39 @@
+export default class {
+    constructor ($scope, $element) {
+        "ngInject";
+
+        this.$scope = $scope;
+        this.$element = $element;
+    }
+
+    $postLink () {
+        // The parent scope of datagrid is required to get parent
+        // values inside cells
+        this.cellScope = this.datagridCtrl.getParentScope().$new(false);
+
+        this.$element[0].style.display = "block";
+
+        this._compileCell();
+    }
+
+    $onChanges (changes) {
+        if (changes.row && !changes.row.isFirstChange()) {
+            this._compileCell();
+        }
+    }
+
+    _compileCell () {
+        this.cellScope.$row = this.row;
+        this.cellScope.$column = this.column;
+        this.cellScope.$value = this.row[this.column.name];
+
+        if (this.column.compiledTemplate) {
+            this.column.compiledTemplate(this.cellScope, clone => {
+                this.$element.empty();
+                this.$element.append(clone);
+            });
+        } else {
+            this.$element[0].innerHTML = this.cellScope.$value;
+        }
+    }
+}
