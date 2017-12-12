299b9802a9f8c9bce80cdd21119b8275b4858ace
Kevin Manson <kevin.manson@corp.ovh.com>
2017-12-12 17:27:44 -0500

    feat(radio): refactor legacy radio

--- packages/oui-radio/src/radio.controller.js
+++ packages/oui-radio/src/radio.controller.js
@@ -1,76 +1,23 @@
-import _ from "lodash";
-
-const INPUT_SELECTOR = "input";
+import { addBooleanParameter } from "@oui-angular/common/component-utils";
 
 export default class {
-    constructor ($scope, $element, $attrs, $transclude) {
+    constructor ($scope, $element, $attrs) {
         "ngInject";
 
-        this.$element = $element;
         this.$scope = $scope;
+        this.$element = $element;
         this.$attrs = $attrs;
-        this.$transclude = $transclude;
-
-        if (this.onChange) {
-            this.onChange = this.onChange.bind(this);
-        }
     }
 
-    registerEventsToParent () {
-        const self = this;
-
-        this.onGroupValueChange(self.group.value);
-
-        this.$element.find(INPUT_SELECTOR).bind("change", function () {
-            self.$scope.$apply(() => self.group.callOnChange(this.value));
-        });
+    $postLink () {
+        this.$element.removeAttr(["id", "name"]);
     }
 
     $onInit () {
-        this.id = `oui-radio-${this.$scope.$id}`;
+        addBooleanParameter(this, "disabled");
 
-        if (_.has(this.$attrs, "checked") && _.isEmpty(this.$attrs.checked)) {
-            this.checked = true;
+        if (!self.id) {
+            this.id = `oui-radio-${this.$scope.$id}`;
         }
-
-        if (_.has(this.$attrs, "disabled") && _.isEmpty(this.$attrs.disabled)) {
-            this.disabled = true;
-        }
-
-        if (this.group) {
-            this.name = this.group.name;
-            this.group.radios.push(this);
-
-            this.registerEventsToParent();
-        } else {
-            this.name = this.id;
-        }
-
-        if (_.has(this.$attrs, "big")) {
-            this.big = true;
-        }
-
-        if (_.has(this.$attrs, "thumbnail")) {
-            this.thumbnail = true;
-        }
-
-        this.hasDescription = Boolean(this.$transclude.isSlotFilled("description") || this.description);
-    }
-
-    $onDestroy () {
-        if (self.group) {
-            _.remove(this.group.radios, this);
-            this.$element.find(INPUT_SELECTOR).unbind("change");
-        }
-    }
-
-    $onChanges (changes) {
-        if (_.has(changes, "checked")) {
-            this.checked = changes.checked.currentValue;
-        }
-    }
-
-    onGroupValueChange (value) {
-        this.checked = value === this.value;
     }
 }
