c47370f4af4c36aafede8b373239bbac7dda994c
Jimmy Fortin <jimmy.fortin@corp.ovh.com>
2018-01-03 12:58:34 -0500

    chore(oui-radio-group): remove legacy component
    Signed-off-by: Jimmy Fortin <jimmy.fortin@corp.ovh.com>

--- packages/oui-radio-group/src/index.js
+++
@@ -1,6 +0,0 @@
-import "@oui-angular/oui-radio/src";
-import RadioGroup from "./radio-group.component.js";
-
-angular.module("oui.radio-group", [
-    "oui.radio"
-]).component("ouiRadioGroup", RadioGroup);
