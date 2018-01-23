5a882141f741d4423fc2385aba6382bd1eaab771
Kevin Manson <kevin.manson@corp.ovh.com>
2018-01-23 09:43:03 -0500

    fix(ouiMessage): add dismissable property to force the close button visibility

--- packages/oui-message/src/message.controller.js
+++ packages/oui-message/src/message.controller.js
@@ -1,4 +1,16 @@
+import { addBooleanParameter } from "@oui-angular/common/component-utils";
+
 export default class {
+    constructor ($attrs) {
+        "ngInject";
+
+        this.$attrs = $attrs;
+    }
+
+    $onInit () {
+        addBooleanParameter(this, "dismissable");
+    }
+
     dismiss () {
         this.dismissed = true;
 
