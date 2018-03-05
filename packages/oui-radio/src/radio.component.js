e27e50a3573df147b04d084dc6e6f1eddba592eb
rfougier <romain.fougier@corp.ovh.com>
2018-03-05 15:34:18 -0500

    feat(oui-radio): add transclude and thumbnail style options

--- packages/oui-radio/src/radio.component.js
+++ packages/oui-radio/src/radio.component.js
@@ -16,6 +16,8 @@ export default {
         name: "@?",
         model: "=?",
         onChange: "&?",
-        disabled: "<?"
-    }
+        disabled: "<?",
+        thumbnail: "<?"
+    },
+    transclude: true
 };
