6778ba62a5a0b2882653bf39d8270687a0c811c6
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2018-03-29 17:42:45 -0400

    test(oui-stepper): add tests

--- packages/oui-stepper/src/index.spec.js
+++ packages/oui-stepper/src/index.spec.js
@@ -16,85 +16,153 @@ describe("ouiStepper", () => {
     }));
 
     describe("Component", () => {
-        it("should affects rootClass to stepper", () => {
-            const element = TestUtils.compileTemplate("<oui-stepper></oui-stepper>");
-            $timeout.flush();
 
-            expect(element.hasClass(rootClass)).toBe(true);
+        describe("StepForm", () => {
+
+            it("should add a custom name and id to step", () => {
+                const element = TestUtils.compileTemplate(`
+                    <oui-stepper>
+                        <oui-step-form name="formName" id="formId"></oui-step-form>
+                    </oui-stepper>`);
+                const form = element.find("form").eq(0);
+                $timeout.flush();
+
+                expect(form.attr("name")).toBe("formName");
+                expect(form.attr("id")).toBe("formId");
+            });
+
+            it("should add a header to step", () => {
+                const title = "my step title";
+                const element = TestUtils.compileTemplate(`
+                    <oui-stepper>
+                        <oui-step-form header="${title}"></oui-step-form>
+                    </oui-stepper>`);
+                const form = element.find("form").eq(0);
+                const span = form.find("span").eq(0);
+                $timeout.flush();
+
+                expect(span.hasClass("oui-list__header")).toBe(true);
+                expect(span.html()).toContain(title);
+            });
+
+            it("should display an active step", () => {
+                const element = TestUtils.compileTemplate("<oui-stepper><oui-step-form></oui-step-form></oui-stepper>");
+                const form = element.find("form").eq(0);
+                $timeout.flush();
+
+                expect(form.hasClass(activeClass)).toBe(true);
+                expect(form.hasClass(disabledClass)).toBe(false);
+                expect(form.hasClass(completeClass)).toBe(false);
+            });
+
+            it("should display a disabled step", () => {
+                const element = TestUtils.compileTemplate("<oui-stepper><oui-step-form disabled></oui-step-form></oui-stepper>");
+                const form = element.find("form").eq(0);
+                $timeout.flush();
+
+                expect(form.hasClass(activeClass)).toBe(true);
+                expect(form.hasClass(disabledClass)).toBe(true);
+                expect(form.hasClass(completeClass)).toBe(false);
+            });
+
+            it("should display a loader", () => {
+                const element = TestUtils.compileTemplate("<oui-stepper><oui-step-form loading></oui-step-form></oui-stepper>");
+                $timeout.flush();
+
+                expect(element.html()).toContain("oui-spinner");
+            });
+
+            it("should display a skippable step", () => {
+                const element = TestUtils.compileTemplate("<oui-stepper><oui-step-form skippable></oui-step-form></oui-stepper>");
+                const defaultText = "Skip this step";
+                $timeout.flush();
+
+                expect(element.html()).toContain("button");
+                const button = element.find("button").eq(0);
+                expect(button.html()).toContain(defaultText);
+            });
+
+            it("should submit a step", () => {
+                const onSubmitSpy = jasmine.createSpy();
+                const element = TestUtils.compileTemplate(`
+                    <oui-stepper>
+                        <oui-step-form name='form' on-submit="$ctrl.onSubmitSpy(form)"></oui-step-form>
+                    </oui-stepper>`, { onSubmitSpy });
+                const form = element.find("form").eq(0);
+                $timeout.flush();
+
+                form.triggerHandler("submit");
+                expect(onSubmitSpy).toHaveBeenCalled();
+            });
         });
 
-        it("should display an active step", () => {
-            const element = TestUtils.compileTemplate("<oui-stepper><oui-step-form></oui-step-form></oui-stepper>");
-            const form = element.find("form").eq(0);
-            $timeout.flush();
+        describe("Stepper", () => {
 
-            expect(form.hasClass(activeClass)).toBe(true);
-            expect(form.hasClass(disabledClass)).toBe(false);
-            expect(form.hasClass(completeClass)).toBe(false);
+            it("should affects rootClass to stepper", () => {
+                const element = TestUtils.compileTemplate("<oui-stepper></oui-stepper>");
+                $timeout.flush();
+
+                expect(element.hasClass(rootClass)).toBe(true);
+            });
+
+            it("should add a custom name and id to step", () => {
+                const element = TestUtils.compileTemplate("<oui-stepper name='stepperName' id='stepperId'></oui-stepper>");
+                const div = element.find("div").eq(0);
+                $timeout.flush();
+
+                expect(div.attr("name")).toBe("stepperName");
+                expect(div.attr("id")).toBe("stepperId");
+            });
+
+            it("should call onInit and onFinish", () => {
+                const onInit = jasmine.createSpy();
+                const onFinish = jasmine.createSpy();
+                const element = TestUtils.compileTemplate(`
+                    <oui-stepper on-init="$ctrl.onInit()" on-finish="$ctrl.onFinish()">
+                        <oui-step-form></oui-step-form>
+                    </oui-stepper>`, {
+                    onInit,
+                    onFinish
+                });
+                $timeout.flush();
+
+                // Initial condition
+                expect(onInit).toHaveBeenCalled();
+                expect(onFinish).not.toHaveBeenCalled();
+
+                const form = element.find("form").eq(0);
+                form.triggerHandler("submit");
+
+                // Final condition
+                expect(onFinish).toHaveBeenCalled();
+
+            });
+
+            it("should have a linear behavior", () => {
+                const onSubmitSpy = jasmine.createSpy();
+                const element = TestUtils.compileTemplate(`
+                    <oui-stepper linear>
+                        <oui-step-form name='form1' on-submit="$ctrl.onSubmitSpy(form1)"></oui-step-form>
+                        <oui-step-form name='form2' on-submit="$ctrl.onSubmitSpy(form2)"></oui-step-form>
+                    </oui-stepper>`, { onSubmitSpy });
+                const form1 = element.find("form").eq(0);
+                const form2 = element.find("form").eq(1);
+                $timeout.flush();
+
+                // Initial condition
+                expect(form1.hasClass(activeClass)).toBe(true);
+                expect(form1.hasClass(disabledClass)).toBe(false);
+                expect(form2.hasClass(disabledClass)).toBe(true);
+
+                form1.triggerHandler("submit");
+
+                // Final condition
+                expect(form1.hasClass(disabledClass)).toBe(true);
+                expect(form2.hasClass(activeClass)).toBe(true);
+                expect(form2.hasClass(disabledClass)).toBe(false);
+            });
+
         });
 
-        it("should display a disabled step", () => {
-            const element = TestUtils.compileTemplate("<oui-stepper><oui-step-form disabled></oui-step-form></oui-stepper>");
-            const form = element.find("form").eq(0);
-            $timeout.flush();
-
-            expect(form.hasClass(activeClass)).toBe(true);
-            expect(form.hasClass(disabledClass)).toBe(true);
-            expect(form.hasClass(completeClass)).toBe(false);
-        });
-
-        it("should display a loader", () => {
-            const element = TestUtils.compileTemplate("<oui-stepper><oui-step-form loading></oui-step-form></oui-stepper>");
-            $timeout.flush();
-
-            expect(element.html()).toContain("oui-spinner");
-        });
-
-        fit("should display a skippable step", () => {
-            const element = TestUtils.compileTemplate("<oui-stepper><oui-step-form skippable></oui-step-form></oui-stepper>");
-            const defaultText = "Skip this step";
-            $timeout.flush();
-
-            expect(element.html()).toContain("button");
-            const button = element.find("button").eq(0);
-            expect(button.html()).toContain(defaultText);
-        });
-
-        it("should submit a step", () => {
-            const onSubmitSpy = jasmine.createSpy();
-            const element = TestUtils.compileTemplate(`
-                <oui-stepper>
-                    <oui-step-form name='form' on-submit="$ctrl.onSubmitSpy(form)"></oui-step-form>
-                </oui-stepper>`, { onSubmitSpy });
-            const form = element.find("form").eq(0);
-            $timeout.flush();
-
-            form.triggerHandler("submit");
-            expect(onSubmitSpy).toHaveBeenCalled();
-        });
-
-        it("should have a linear behavior", () => {
-            const onSubmitSpy = jasmine.createSpy();
-            const element = TestUtils.compileTemplate(`
-                <oui-stepper linear>
-                    <oui-step-form name='form1' on-submit="$ctrl.onSubmitSpy(form1)"></oui-step-form>
-                    <oui-step-form name='form2' on-submit="$ctrl.onSubmitSpy(form2)"></oui-step-form>
-                </oui-stepper>`, { onSubmitSpy });
-            const form1 = element.find("form").eq(0);
-            const form2 = element.find("form").eq(1);
-            $timeout.flush();
-
-            // Initial condition
-            expect(form1.hasClass(activeClass)).toBe(true);
-            expect(form1.hasClass(disabledClass)).toBe(false);
-            expect(form2.hasClass(disabledClass)).toBe(true);
-
-            form1.triggerHandler("submit");
-
-            // Final condition
-            expect(form1.hasClass(disabledClass)).toBe(true);
-            expect(form2.hasClass(activeClass)).toBe(true);
-            expect(form2.hasClass(disabledClass)).toBe(false);
-        });
     });
 });
