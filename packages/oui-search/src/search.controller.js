f2597896193aea9842a087494c81a59369c47174
Kevin Manson <kevin.manson@corp.ovh.com>
2018-04-11 10:18:01 -0400

    feat(oui-search): add debouncer on search component

--- packages/oui-search/src/search.controller.js
+++ packages/oui-search/src/search.controller.js
@@ -1,9 +1,13 @@
 import { addBooleanParameter } from "@oui-angular/common/component-utils";
+import { debounce } from "lodash";
 
 const componentClass = "oui-search";
 
 // Min length needed to create a new criterion.
 const minLengthTrigger = 2;
+
+// Minimum delay before each criterion change.
+const criterionDebounceDelay = 800;
 
 export default class SearchController {
     constructor ($attrs, $element, $timeout) {
@@ -12,6 +16,10 @@ export default class SearchController {
         this.$attrs = $attrs;
         this.$element = $element;
         this.$timeout = $timeout;
+
+        this.onCriterionChange = debounce(this.onCriterionChange.bind(this), criterionDebounceDelay);
+        this.onCriterionSubmit = debounce(this.onCriterionSubmit.bind(this), criterionDebounceDelay);
+        this.onCriterionReset = debounce(this.onCriterionReset.bind(this), criterionDebounceDelay);
     }
 
     $onInit () {
@@ -36,6 +44,12 @@ export default class SearchController {
 
         this.onChange({ modelValue });
 
+        this.onCriterionChange();
+    }
+
+    onCriterionChange () {
+        const modelValue = this.model;
+
         if (this.criteriaContainer) {
             if (modelValue.length >= minLengthTrigger) {
                 this.criteriaContainer.setPreviewCriterion(SearchController.getCriterion(modelValue), true);
@@ -50,6 +64,10 @@ export default class SearchController {
 
         this.onSubmit({ modelValue });
 
+        this.onCriterionSubmit(modelValue);
+    }
+
+    onCriterionSubmit (modelValue) {
         if (this.criteriaContainer && modelValue.length >= minLengthTrigger) {
             this.criteriaContainer.add(SearchController.getCriterion(modelValue));
         }
@@ -61,6 +79,10 @@ export default class SearchController {
 
         this.onReset();
 
+        this.onCriterionReset();
+    }
+
+    onCriterionReset () {
         if (this.criteriaContainer) {
             this.criteriaContainer.deletePreviewCriterion();
         }
