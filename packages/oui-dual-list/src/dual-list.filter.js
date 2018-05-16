a1d7edfd74808f831855fa239e08f3c4524a6dc6
Ravindra Adireddy <ravindra.adireddy@corp.ovh.com>
2018-05-16 11:35:23 +0530

    used loadsh method to get nested property

--- packages/oui-dual-list/src/dual-list.filter.js
+++ packages/oui-dual-list/src/dual-list.filter.js
@@ -1,10 +1,11 @@
+import { get } from "lodash";
 export default function (items, sourceFilterKey, property) {
     return items.filter(item => {
         let value;
         if (!property) {
             value = item;
         } else {
-            value = property.split(".").reduce((prev, curr) => prev ? prev[curr] : undefined, item);
+            value = get(item, property, item);
         }
         return value.match(new RegExp(sourceFilterKey, "i"));
     });
