aa29fa45840c5eb639cf28b799c35a2f18b87187
Axel Peter <axel.peter@corp.ovh.com>
2018-05-28 14:41:49 +0200

    chore(oui-skeleton): add  flush

--- packages/oui-skeleton/src/index.spec.js
+++ packages/oui-skeleton/src/index.spec.js
@@ -1,11 +1,13 @@
 describe("ouiSkeleton", () => {
     let TestUtils;
+    let $timeout;
 
     beforeEach(angular.mock.module("oui.skeleton"));
     beforeEach(angular.mock.module("oui.test-utils"));
 
-    beforeEach(inject(_TestUtils_ => {
+    beforeEach(inject((_TestUtils_, _$timeout_) => {
         TestUtils = _TestUtils_;
+        $timeout = _$timeout_;
     }));
 
     describe("Component", () => {
@@ -16,6 +18,8 @@ describe("ouiSkeleton", () => {
 
         it("should display a oui skeleton div", () => {
             const element = TestUtils.compileTemplate("<oui-skeleton size=\"m\"></oui-skeleton");
+
+            $timeout.flush();
             expect(angular.element(element[0].querySelector(".oui-skeleton_m"))).toBeTruthy();
         });
     });
