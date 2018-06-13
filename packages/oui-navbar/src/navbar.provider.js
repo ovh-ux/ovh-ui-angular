22853f530498a695bebe7ca70f7c0c3b778dffa7
Jean-Daniel Pearson <1476965+JDownloader@users.noreply.github.com>
2018-06-13 04:11:09 -0400

    feat(oui-navbar): add placeholder for notification (#199)
    * feat(oui-navbar): add placeholder for notification
    
    - show a message when no notification or when something went wong
    - add incident level for notifiacation
    
    * refactor(oui-navbar): modify class for notification

--- packages/oui-navbar/src/navbar.provider.js
+++ packages/oui-navbar/src/navbar.provider.js
@@ -2,8 +2,12 @@ export default class {
     constructor () {
         this.translations = {
             notification: {
+                errorInNotification: "Oups, there’s an error!",
+                errorInNotificationDescription: "We can’t initialize the menu.",
                 markRead: "Mark as read",
-                markUnread: "Mark as unread"
+                markUnread: "Mark as unread",
+                noNotification: "You are all caught up!",
+                noNotificationDescription: "You don't have any notification."
             }
         };
     }
