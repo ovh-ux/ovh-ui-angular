792ae772d2d565f94333eb8540f987671d9fca6d
Romain Fougier <31250722+rfougier@users.noreply.github.com>
2018-06-26 09:40:22 -0400

    fix(oui-header-tabs): add active class when dropdown has an active item (#212)

--- packages/oui-header-tabs/src/header-tabs-dropdown.component.js
+++ packages/oui-header-tabs/src/header-tabs-dropdown.component.js
@@ -1,22 +1,34 @@
 import template from "./header-tabs-dropdown.html";
 
+const itemClass = "oui-header-tabs__item";
+const dropDownClass = `${itemClass}_dropdown`;
+
 export default {
     bindings: {
         text: "@"
     },
     controller: class {
-        constructor ($element, $timeout) {
+        constructor ($element, $scope, $timeout) {
             "ngInject";
 
             this.$element = $element;
+            this.$scope = $scope;
             this.$timeout = $timeout;
         }
 
         $postLink () {
             this.$timeout(() => {
                 this.$element
-                    .addClass("oui-header-tabs__item oui-header-tabs__item_dropdown")
+                    .addClass(`${itemClass} ${dropDownClass}`)
                     .attr("role", "listitem");
+
+                this.$scope.$watch(() => !!this.$element[0].querySelector(`.${itemClass}_active`), hasActive => {
+                    if (hasActive) {
+                        this.$element.addClass(`${dropDownClass}_active`);
+                    } else {
+                        this.$element.removeClass(`${dropDownClass}_active`);
+                    }
+                });
             });
         }
     },
