cbb7f2a3dd088d54dd78622b41956ec2257bdf35
Marie JONES <14836007+marie-j@users.noreply.github.com>
2018-07-17 16:48:37 +0200

    fix(oui-action-menu): allow to disable action menu dynamically (#232)

--- packages/oui-action-menu/src/action-menu.component.js
+++ packages/oui-action-menu/src/action-menu.component.js
@@ -8,7 +8,8 @@ export default {
         text: "@",
         align: "@?",
         ariaLabel: "@?",
-        compact: "<?"
+        compact: "<?",
+        disabled: "<?"
     },
     transclude: true
 };
