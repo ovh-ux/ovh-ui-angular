5928ec0fb17565b9ebde888cf10a2dbc60aa5ad9
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2018-07-18 10:02:59 +0200

    feat(oui-tile): add external link handle

--- packages/oui-tile/src/button/tile-button.component.js
+++ packages/oui-tile/src/button/tile-button.component.js
@@ -8,7 +8,8 @@ export default {
         text: "@?",
         onClick: "&?",
         href: "@?",
-        ariaLabel: "@?"
+        ariaLabel: "@?",
+        external: "<?"
     },
     transclude: true
 };
