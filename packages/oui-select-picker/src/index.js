3a7ad05cd0e6394e498569f76cb61afa2485f0bb
Axel Peter <15101925+AxelPeter@users.noreply.github.com>
2018-11-12 10:42:40 +0100

    fix(oui-select-picker): fix transclude with component instead of span (#312)

--- packages/oui-select-picker/src/index.js
+++ packages/oui-select-picker/src/index.js
@@ -1,6 +1,8 @@
-import SelectPicker from "./select-picker.component.js";
+import SelectPicker from "./select-picker.component";
+import SelectPickerSection from "./section/select-picker-section.component";
 
 export default angular
     .module("oui.select-picker", [])
     .component("ouiSelectPicker", SelectPicker)
+    .component("ouiSelectPickerSection", SelectPickerSection)
     .name;
