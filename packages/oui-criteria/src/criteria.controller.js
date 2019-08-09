b9fecc56ff538df3d74c9b6bc66582b52ebeb088
Axel Peter <axel.peter@live.com>
2019-08-09 13:10:26 +0200

    fix(oui-datagrid): fix preview search shouldn't trigger onCriteriaChange

--- packages/oui-criteria/src/criteria.controller.js
+++ packages/oui-criteria/src/criteria.controller.js
@@ -17,16 +17,18 @@ export default class {
         this.debounceDelay = 500;
     }
 
-    triggerChange (force = false) {
-        if (this.onChange || force) {
-            this.onChange({ modelValue: angular.copy(this.model) });
-            this.criteria = this.model
-                .filter(criterion => !criterion.preview)
-                .map((criterion) => ({
-                    title: this.buildTitle(criterion),
-                    ...criterion
-                }));
-        }
+    triggerChange (preview) {
+        this.onChange({
+            modelValue: angular.copy(this.model),
+            preview
+        });
+
+        this.criteria = this.model
+            .filter(criterion => !criterion.preview)
+            .map(criterion => ({
+                title: this.buildTitle(criterion),
+                ...criterion
+            }));
     }
 
     indexOfCriterion (criterion) {
@@ -45,14 +47,16 @@ export default class {
         } else {
             this.model.push(previewCriterion);
         }
-        this.triggerChange();
+        this.triggerChange(true);
     }
 
-    deletePreviewCriterion () {
+    deletePreviewCriterion (preview) {
         const previewCriterionIndex = findIndex(this.model, ["preview", true]);
         if (previewCriterionIndex > -1) {
             this.model.splice(previewCriterionIndex, 1);
-            this.triggerChange();
+
+            // We set the search reset as a preview
+            this.triggerChange(preview);
         }
     }
 
@@ -89,7 +93,7 @@ export default class {
         if (criterionIndex > -1) {
             this.model.splice(criterionIndex, 1);
         }
-        this.triggerChange(true);
+        this.triggerChange();
     }
 
     set (criteria) {
@@ -106,12 +110,12 @@ export default class {
         if (modelValue && modelValue.length >= this.minLength) {
             this.setPreviewCriterion(this.constructor.getCriterion(modelValue), true);
         } else {
-            this.deletePreviewCriterion();
+            this.deletePreviewCriterion(true);
         }
     }
 
     onCriterionReset () {
-        this.deletePreviewCriterion();
+        this.deletePreviewCriterion(true);
     }
 
     onCriterionSubmit (modelValue) {
